11bd583c9019d71798231f860ac20afbdfa134b2
jambrizz <ambrizjovani@gmail.com>
2022-09-23 00:01:46 -0700

    Portfolio revision

--- main.js
+++ main.js
@@ -1,20 +1,28 @@
+//array of links for each week
 const links = [
     {
         label: "Week 1 Notes",
-        url: "week1/index.html"
+        url: "week01/index.html"
     }
 ]
 
+//loop through the array above and create a list item for each link
 function displayLinks() {
-    const ol = document.createElement("ol");
-    for (let i = 0; i < links.length; i++) {
-        const li = document.createElement("li");
-        const a = document.createElement("a");
-        a.setAttribute("href", links[i].url);
-        a.textContent = links[i].label;
-        li.appendChild(a);
-        ol.appendChild(li);
-    }
-    document.getElementById("weeks").innerHTML = ol.innerHTML;
+    //variable to hold the list items
+    const ol = document.querySelector("#weeks");
 
+    //loop through the array
+    links.forEach( link => {
+        //create a list item
+        const li = document.createElement("li");
+        //create an anchor tag
+        const href = document.createElement("a");
+        //set the href attribute
+        href.setAttribute("href", link.url);
+        //set the text label
+        href.innerText = link.label;
+
+        li.appendChild(href);
+        ol.appendChild(li);
+    })    
 }
