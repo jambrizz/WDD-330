cfe47e3918bea23c3b3a3b1aa6201444eec04261
jambrizz <ambrizjovani@gmail.com>
2022-12-12 00:56:55 -0800

    battleship draft1

--- Block2Challenge/index.js
+++ Block2Challenge/index.js
@@ -1,6 +1,9 @@
 //******************************  view ********************************* */
 let gameBoard = document.getElementById('board');
+let shipPlacement = document.getElementById('players-ships');
 createBoard(6,6);
+playersBoards(6,6);
+
 
 function createBoard(col, row) {
     let table = document.createElement('table');
@@ -22,11 +25,32 @@ function createBoard(col, row) {
     gameBoard.appendChild(table);
     generateShips();
 };
-/***************************************************************8 */
+
+function playersBoards(col, row) {
+    let table = document.createElement('table');
+    table.setAttribute('id', 'shipsGrid');
+    let count = 0;
+    for(let i = 0; i < col; i++) {
+        let tRow = document.createElement('tr');
+        for(let j = 0; j < row; j++) {
+            let tCol = document.createElement('td');
+            tCol.setAttribute('class', 'cell');
+            tCol.setAttribute('id', `${count}`);
+            tCol.setAttribute('value', 'false');
+            count++;
+            tRow.appendChild(tCol);
+        }
+        table.appendChild(tRow);
+    }
+    shipPlacement.appendChild(table);
+};
+
+
+/****************************************************************/
 
 //******************************  model ********************************* */
 //generate ships
-
+const totalShips = 5;
 function generateShips() {
     //const location = Math.floor(Math.random() * 35);
     //let i = 0;
@@ -43,21 +67,55 @@ function generateShips() {
     };
 };
 
-function hit(event) {
-    const cell = event.target.id;
-    const cellValue = document.getElementById(cell).getAttribute('value');
-    const targetCell = document.getElementById(cell);
-    if(cellValue == 'false') {
-        targetCell.textContent = 'X';
-    } else {
-        targetCell.textContent = 'Hit';
-    };
-    //const cellId = document.getElementById(cell);
-    //console.log(cellId);
-};
-
 /****************************************************************/
 
 
 //******************************  controller ********************************* */
-gameBoard.addEventListener('click', hit);
+let battleships = 5;
+let hits = 0;
+
+let playersBattleships = 0;
+let computerHits = 0;
+
+grid.addEventListener('click', hit);
+shipsGrid.addEventListener('click', playerPlacementOfShips);
+
+let shipsArray = [];
+
+function playerPlacementOfShips(event) {
+    const cell = event.target.id;
+    const cellValue = document.getElementById(cell).getAttribute('value');
+    const targetCell = document.getElementById(cell);
+        if(cellValue == 'false') {
+            //valueCell.setAttribute('value', 'true');
+            //targetCell.setAttribute('value', 'true');
+            shipsArray.push(id, `${targetCell.id}`, value, true);
+            console.log(shipsArray);
+            ++playersBattleships;
+            console.log(playersBattleships);
+            if(playersBattleships == 5) {
+                console.log(shipsArray);
+            }
+        }
+};
+
+function hit(event) {
+    const cell = event.target.id;
+    const cellValue = document.getElementById(cell).getAttribute('value');
+    const targetCell = document.getElementById(cell);
+        if(cellValue == 'true') {
+            targetCell.textContent = 'Hit';
+            targetCell.style = "background-color: red;"
+            ++hits;
+        } else {
+            targetCell.textContent = 'X';
+        };
+    checkForWin();
+};
+
+function checkForWin() {
+    if(hits == battleships) {
+        alert('you won');
+    }
+};
+
